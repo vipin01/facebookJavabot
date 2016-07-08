236b4d8d4400e4f8d83c2b356ad6cf9b01758e3f
prash1012000 <prash101_2000@yahoo.com>
2016-07-08 10:09:00 +0530

    changes to include template

--- src/main/java/com/barclays/facebook/bot/JavaAPIBotServlet.java
+++ src/main/java/com/barclays/facebook/bot/JavaAPIBotServlet.java
@@ -21,9 +21,13 @@ import com.restfb.FacebookClient.AccessToken;
 import com.restfb.Parameter;
 import com.restfb.Version;
 import com.restfb.types.GraphResponse;
-
+import com.restfb.types.send.Bubble;
+import com.restfb.types.send.ButtonTemplatePayload;
+import com.restfb.types.send.GenericTemplatePayload;
 import com.restfb.types.send.IdMessageRecipient;
 import com.restfb.types.send.Message;
+import com.restfb.types.send.PostbackButton;
+import com.restfb.types.send.TemplateAttachment;
 import com.restfb.types.webhook.WebhookEntry;
 import com.restfb.types.webhook.WebhookObject;
 import com.restfb.types.webhook.messaging.MessagingItem;
@@ -33,103 +37,168 @@ import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 
-@WebServlet(name = "/webhook", urlPatterns = { "/webhook" })
+//@WebServlet(name = "/webhook", urlPatterns = { "/webhook" })
 public class JavaAPIBotServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
-    protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
-            throws ServletException, IOException {
-        System.out.println("inside doGet() method");
-       // response.getWriter().println("hello");
+	protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("inside doGet() method");
+		// response.getWriter().println("hello");
 
-       if (request.getParameter("hub.verify_token").equalsIgnoreCase("verify_token")) {
-            response.getWriter().append(request.getParameter("hub.challenge"));
-        } else {
-            response.getWriter().append("Served at: ").append(request.getContextPath());
-        }
-    }
-	
+		if (request.getParameter("hub.verify_token").equalsIgnoreCase("verify_token")) {
+			// if
+			// (request.getParameter("hub.verify_token").equalsIgnoreCase("verify_code"))
+			// {
+			response.getWriter().append(request.getParameter("hub.challenge"));
+		} else {
+			response.getWriter().append("Served at: ").append(request.getContextPath());
+		}
+	}
+
 	@Override
-	  protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
-	      throws ServletException, IOException {
+	protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
+			throws ServletException, IOException {
 
-	    // retrieve POST Body
-	    String body = request.getReader().lines().reduce("", (accumulator, actual) -> accumulator + actual);
+		// retrieve POST Body
+		String body = request.getReader().lines().reduce("", (accumulator, actual) -> accumulator + actual);
 
-	    // map body json to WebhookObject
-	    DefaultJsonMapper mapper = new DefaultJsonMapper();
-	    WebhookObject webhookObject = mapper.toJavaObject(body, WebhookObject.class);
+		// map body json to WebhookObject
+		DefaultJsonMapper mapper = new DefaultJsonMapper();
+		WebhookObject webhookObject = mapper.toJavaObject(body, WebhookObject.class);
 
-	    for (WebhookEntry entry : webhookObject.getEntryList()) {
-	      if (!entry.getMessaging().isEmpty()) {
-	        for (MessagingItem item : entry.getMessaging()) {
-	          String senderId = item.getSender().getId();
+		for (WebhookEntry entry : webhookObject.getEntryList()) {
+			if (!entry.getMessaging().isEmpty()) {
+				for (MessagingItem item : entry.getMessaging()) {
+					String senderId = item.getSender().getId();
 
-	          // create recipient
-	          IdMessageRecipient recipient = new IdMessageRecipient(senderId);
-	         if( item.getMessage()!=null){
-	          String message=fetchQueryResponse(item.getMessage().getText());
+					// create recipient
+					IdMessageRecipient recipient = new IdMessageRecipient(senderId);
+					if (item.getMessage() != null) {
+						String message = fetchQueryResponse(item.getMessage().getText());
 
-	          final Gson gson = GsonFactory.getGson();
-	          final AIResponse aiResponse = gson.fromJson(message, AIResponse.class);
-	          // check message
-	          if (item.getMessage() != null && item.getMessage().getText() != null) {
-	            // create simple text message
-	            Message simpleTextMessage = new Message(aiResponse.getResult().getFulfillment().getSpeech());
+						final Gson gson = GsonFactory.getGson();
+						final AIResponse aiResponse = gson.fromJson(message, AIResponse.class);
+						// check message
+						if (item.getMessage() != null && item.getMessage().getText() != null) {
+							// create simple text message
+							// Message simpleTextMessage = new
+							// Message(aiResponse.getResult().getFulfillment().getSpeech());
+							String temp = aiResponse.getResult().getFulfillment().getSpeech();
+							System.out.printf("Temp %s", temp);
+							if (temp == null || temp.isEmpty()) {
+								System.out.println("Temp is empty");
+								String message2 = fetchQueryResponse("none");
+								final AIResponse aiResponse2 = gson.fromJson(message2, AIResponse.class);
+								temp = aiResponse2.getResult().getFulfillment().getSpeech();
+							}
+							Message templateMessage = getTemplateMessage(temp);
+							// build send client and send message
+							FacebookClient sendClient = new DefaultFacebookClient(
+									"EAAOlIyo3LTMBAApctladjzZAoZCsInwCe2QI7IbeFGFtWSYe3dWAAogZBYjdRLmjsQGfPAMLZB9FBCJSJK2VqdkKkZAkivlplQLeU3d98xstN84VIEpJ4rP5KmnC9IKUwR7oSY83knB0LYMZBOQ87R9GkWMDZBeRJzZApbfqjBb4YQZDZD",
+									Version.VERSION_2_6);
+							// FacebookClient sendClient = new
+							// DefaultFacebookClient("EAAG3bIDZBeuEBAOJkn2qxfB9w5ZByTIoa2RHCFFP1uGYD9yX9aFK8OscnjwAk4mjIy0hbhzWrZCZC1VRlCoRHl5Pkz7XxZCp31Ajx9aNoAKojjASIzmxsdNEVJtpSYhF5OnIboCFjEwwBGxZCMur0cXREaRoaCbssUypZCyVICeQwZDZD",
+							// Version.VERSION_2_6);
+							sendClient.publish("me/messages", GraphResponse.class,
+									Parameter.with("recipient", recipient), Parameter.with("message", templateMessage));
+						}
 
-	            // build send client and send message
-	            FacebookClient sendClient = new DefaultFacebookClient("EAAOZB9RlT1RsBAA8UakMpEDlT5mwHWHOBZC1qBiq5tSLu9bouLZCsmwPOZBxW2CakWlFXz19rCTqXp1VeUpi4uPwWkCfr9LeX8bFr2uYXsec1mVLmeaEsZB2sWfUP1aZCDrWkfjuqNQayoNy5nvOq1zderQYu1OsI1PMcYDwfhRQZDZD", Version.VERSION_2_6);
-	            sendClient.publish("me/messages", GraphResponse.class, Parameter.with("recipient", recipient),
-	              Parameter.with("message", simpleTextMessage));
-	          }
+					}
+					if (item.getPostback() != null) {
+						String message = fetchQueryResponse(item.getPostback().getPayload());
 
-	          if (item.getPostback() != null) {
-	            //LOG.debug("run postback");
-	          }
-	         }
-	        }
-	      }
-	    }
-	  }
-	
-	public String fetchQueryResponse(String query){
+						final Gson gson = GsonFactory.getGson();
+						final AIResponse aiResponse = gson.fromJson(message, AIResponse.class);
+						String temp = aiResponse.getResult().getFulfillment().getSpeech();
+						System.out.printf("Temp %s", temp);
+						if (temp == null || temp.isEmpty()) {
+							System.out.println("Temp is empty");
+							String message2 = fetchQueryResponse("none");
+							final AIResponse aiResponse2 = gson.fromJson(message2, AIResponse.class);
+							temp = aiResponse2.getResult().getFulfillment().getSpeech();
+						}
+						Message templateMessage = getTemplateMessage(temp);
 
-        String url="https://api.api.ai/v1/query";
+						// build send client and send message
+						FacebookClient sendClient = new DefaultFacebookClient(
+								"EAAOlIyo3LTMBAApctladjzZAoZCsInwCe2QI7IbeFGFtWSYe3dWAAogZBYjdRLmjsQGfPAMLZB9FBCJSJK2VqdkKkZAkivlplQLeU3d98xstN84VIEpJ4rP5KmnC9IKUwR7oSY83knB0LYMZBOQ87R9GkWMDZBeRJzZApbfqjBb4YQZDZD",
+								Version.VERSION_2_6);
+						// FacebookClient sendClient = new
+						// DefaultFacebookClient("EAAG3bIDZBeuEBAOJkn2qxfB9w5ZByTIoa2RHCFFP1uGYD9yX9aFK8OscnjwAk4mjIy0hbhzWrZCZC1VRlCoRHl5Pkz7XxZCp31Ajx9aNoAKojjASIzmxsdNEVJtpSYhF5OnIboCFjEwwBGxZCMur0cXREaRoaCbssUypZCyVICeQwZDZD",
+						// Version.VERSION_2_6);
+						sendClient.publish("me/messages", GraphResponse.class, Parameter.with("recipient", recipient),
+								Parameter.with("message", templateMessage));
+						// }
+					}
+				}
+			}
+		}
+	}
 
-        Client client = Client.create(new DefaultClientConfig());
+	public String fetchQueryResponse(String query) {
 
-        MultivaluedMap queryParams = new MultivaluedMapImpl();
+		String url = "https://api.api.ai/v1/query";
 
-        queryParams.add("query", query);
-        queryParams.add("v", "20160621");
-        queryParams.add("timezone", "Europe/London");
-        queryParams.add("lang","en");
-        queryParams.add("sessionId","1234567890");
+		Client client = Client.create(new DefaultClientConfig());
 
-        WebResource webResource = client.resource(url);         
+		MultivaluedMap queryParams = new MultivaluedMapImpl();
 
-        WebResource.Builder builder = webResource.queryParams(queryParams).accept("*/*");
+		queryParams.add("query", query);
+		queryParams.add("v", "20160621");
+		queryParams.add("timezone", "Europe/London");
+		queryParams.add("lang", "en");
+		queryParams.add("sessionId", "1234567890");
 
-        builder.header(HttpHeaders.AUTHORIZATION, "Bearer "+"44bb669ab90341e39af262754f7d5368");
+		WebResource webResource = client.resource(url);
 
-        
+		WebResource.Builder builder = webResource.queryParams(queryParams).accept("*/*");
 
-        ClientResponse response=builder.get(ClientResponse.class);
+		builder.header(HttpHeaders.AUTHORIZATION, "Bearer " + "615edc6548374cc9a9a0672223be7fe4");
+		// builder.header(HttpHeaders.AUTHORIZATION, "Bearer
+		// "+"e278cd5e83e54ca2983d8bbf750a45b7");
 
-        System.out.println("Response is "+response.toString());
-      
-        String entity = response.getEntity(String.class);
+		ClientResponse response = builder.get(ClientResponse.class);
 
-        System.out.println("Response data is : " + entity);
+		System.out.println("Response is " + response.toString());
 
+		String entity = response.getEntity(String.class);
 
-        return entity;                              
+		System.out.println("Response data is : " + entity);
 
-        
+		return entity;
 
-}
+	}
 
+	public Message getTemplateMessage(String textMessage) {
+		GenericTemplatePayload genericPayload = null;
+		TemplateAttachment template = null;
+		Message message = null;
+		String[] splitMessage = textMessage.split(":");
+		if (splitMessage.length > 1) {
+			genericPayload = new GenericTemplatePayload();
+			for (String msg : splitMessage) {
+				PostbackButton postback = new PostbackButton("Answer", msg);
+				Bubble bubble = new Bubble("Welcome to Barclaycard FAQs");
+				// bubble.setImageUrl("http://ba41c57c.ngrok.io/facebookJavabot/images/barclaycard-logo.png");
+				bubble.setImageUrl("http://53bde1b4.ngrok.io/facebookJavabot-0.0.1-SNAPSHOT/images/barclaycard_logo.png");
+				bubble.setItemUrl("http://help.barclaycard.co.uk/brochure/account_services/online-pin/");
+				bubble.setSubtitle(msg);
+				bubble.addButton(postback);
+				genericPayload.addBubble(bubble);
+			}
+			template = new TemplateAttachment(genericPayload);
+			message = new Message(template);
+		} else {
+			if (textMessage.length() > 320)
+				message = new Message(textMessage.substring(0, 320));
+			else
+				message = new Message(textMessage);
+		}
+		return message;
+
+	}
 
 }
